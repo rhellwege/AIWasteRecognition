d3142021368e3577ceafa417719be06d64eb707a
Ryan Hellwege <rhellwege6@gmail.com>
2023-11-02 12:25:40 -0700

    helpful error message

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
-from flask import Flask
 import flask
 import json
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def root_endpoint():
     return flask.render_template('index.html') 
+
+# TODO: add endpoints for upload image, and correct model
 
 if __name__== '__main__':
     app.run(debug=True)
--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 
 if not torch.cuda.is_available():
     print("CUDA is not enabled on your system. Please enable it to train the model.")
+    print("Download the right version of pytorch here https://download.pytorch.org/whl/cu118")
     exit(1)
 
 print(f"CUDA is enabled on this system.")
@@ -10,7 +11,6 @@ print(f"CUDA version: {torch.version.cuda}")
 # Storing ID of current CUDA device
 cuda_id = torch.cuda.current_device()
 print(f"ID of current CUDA device: {torch.cuda.current_device()}")
-       
 print(f"Name of current CUDA device: {torch.cuda.get_device_name(cuda_id)}")
 
 
